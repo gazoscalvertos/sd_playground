b580bb86a83f6ab1fa4264edb9d1cdd011620cfd
gazoscalvertos <gaz.calvert@gmail.com>
2024-10-18 18:27:43 +0100

    Update flux_provisioning.sh

--- flux_provisioning.sh
+++ flux_provisioning.sh
@@ -54,11 +54,12 @@ download_models_from_config() {
     mkdir -p models clip lora vae unet
 
     # Download models from various sections
-    download_model_type "esrgan_models" "models" "$config_file" "$hf_token" "$civitai_token"
-    download_model_type "unet_models" "unet" "$config_file" "$hf_token" "$civitai_token"
-    download_model_type "vae_models" "vae" "$config_file" "$hf_token" "$civitai_token"
-    download_model_type "lora_models" "lora" "$config_file" "$hf_token" "$civitai_token"
-    download_model_type "clip_models" "clip" "$config_file" "$hf_token" "$civitai_token"
+    download_model_type "esrgan_models" "${WORKSPACE}/storage/stable_diffusion/models/models" "$config_file" "$hf_token" "$civitai_token"
+    download_model_type "unet_models" "${WORKSPACE}/storage/stable_diffusion/models/unet" "$config_file" "$hf_token" "$civitai_token"
+    download_model_type "vae_models" "${WORKSPACE}/storage/stable_diffusion/models/vae" "$config_file" "$hf_token" "$civitai_token"
+    download_model_type "lora_models" "${WORKSPACE}/storage/stable_diffusion/models/lora" "$config_file" "$hf_token" "$civitai_token"
+    download_model_type "clip_models" "${WORKSPACE}/storage/stable_diffusion/models/clip" "$config_file" "$hf_token" "$civitai_token"
+    download_model_type "controlnet" "${WORKSPACE}/storage/stable_diffusion/models/controlnet" "$config_file" "$hf_token" "$civitai_token"
 }
 
 # Function to download a specific type of model
